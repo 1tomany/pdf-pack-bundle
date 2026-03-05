3a8f38a3b51e67cb53700958476c6197224f9c09
Vic Cherubini <vcherubini@gmail.com>
2026-03-04 18:10:26 -0600

    wip

--- src/OneToManyPdfPackBundle.php
+++ src/OneToManyPdfPackBundle.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace OneToMany\PDFAIBundle;
+namespace OneToMany\PdfPackBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class PDFAIBundle extends AbstractBundle
+class OneToManyPdfPackBundle extends AbstractBundle
 {
     /**
      * @param array<string, mixed> $config
